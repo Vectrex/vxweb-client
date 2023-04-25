4919d6540b484d79ed22a187d022a9603a301618
Gregor Kofler <info@gregorkofler.com>
2023-04-26 00:47:11 +0200

    Dist filenames are generated without hashes; added a fallback backend host address

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
         manualChunks: {
         }
       }
