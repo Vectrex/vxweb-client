cd526f83214721ed064762c14bee379f82899c9f
gregor <gregor@mail.agindo.intern>
2023-04-27 15:21:02 +0200

    Fix: nested routes don't highlight the parent menu item

--- src/components/app/MainMenu.vue
+++ src/components/app/MainMenu.vue
@@ -16,7 +16,7 @@
           :to="route.name ? { name: route.name } : { path: route.path }"
           :class="[
               'flex items-center px-2 py-2 text-base font-medium rounded items-center space-x-2 flex-nowrap overflow-hidden',
-              $route.name === route.name ? 'bg-vxvue-600 text-white' : 'text-slate-100 hover:bg-vxvue-700'
+              $route.matched[0].path === route.path ? 'bg-vxvue-600 text-white' : 'text-slate-100 hover:bg-vxvue-700'
           ]"
       >
         <component :is="route.meta.icon" v-if="route.meta.icon" class="w-8 h-8 flex-shrink-0" />
