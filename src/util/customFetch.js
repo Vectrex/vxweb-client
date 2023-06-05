7af900f33b35413981a485050b0b602d070529d9
Gregor Kofler <info@gregorkofler.com>
2023-06-05 14:18:38 +0200

    Some guards introduced to custom fetch

--- src/util/customFetch.js
+++ src/util/customFetch.js
@@ -1,20 +1,25 @@
 import { createFetch } from "@vueuse/core";
-import {inject} from "vue";
+import router from "@/router";
+
 export const customFetch = createFetch({
     baseUrl: import.meta.env.VITE_API_ROOT || ('//' + window.location.host + '/admin/'),
     options: {
-        beforeFetch ( { options }) {
+        beforeFetch ({ options }) {
             const bearerToken = sessionStorage.getItem("bearerToken");
             if (bearerToken) {
                 options.headers.Authorization = `Bearer ${bearerToken}`;
             }
             return options;
         },
-        afterFetch (context) {
-            if(context.response.headers.get('bearerToken')) {
-                sessionStorage.setItem('bearerToken', context.response.headers.get('bearerToken'));
+        afterFetch (ctx) {
+            if(ctx.response.headers.get('bearerToken')) {
+                sessionStorage.setItem('bearerToken', ctx.response.headers.get('bearerToken'));
             }
-            return context;
+            return ctx;
+        },
+        onFetchError (ctx) {
+            router.replace({ name: 'authFailed' });
+            return ctx;
         }
     },
     fetchOptions: {
