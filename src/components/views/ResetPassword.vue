947d4eb2e92014b7e150ad5a7d9b351b36850bd0
Gregor Kofler <info@gregorkofler.com>
2023-10-24 21:58:04 +0200

    Reset password view completed

--- src/components/views/ResetPassword.vue
+++ src/components/views/ResetPassword.vue
@@ -5,13 +5,18 @@
   import { ref } from "vue"
 
   const emit = defineEmits(['notify'])
+  const success = ref(false)
   const props = defineProps({ hash: String })
   const form = ref({ password: '', passwordRepeat: '' })
   const submit = async () => {
     if(form.value.password.length >= 8 && form.value.password === form.value.passwordRepeat) {
-      const response = await ((vxFetch('/set-password/' + props.hash))().put(JSON.stringify(form.value)).json()).data.value
-      emit('notify', response)
-      console.log(response)
+      const response = (await vxFetch()('set-password/' + props.hash).put(JSON.stringify(form.value)).json()).data.value
+      if (response.success) {
+        success.value = true
+      }
+      else {
+        emit('notify', response)
+      }
     }
   }
 </script>
@@ -19,17 +24,23 @@
 <template>
   <div class="flex flex-col justify-center py-12 min-h-screen sm:px-6 lg:px-8 bg-slate-200">
     <div class="p-8 ring-1 ring-black ring-opacity-5 shadow sm:mx-auto sm:w-full sm:max-w-md bg-white flex flex-col space-y-4">
-      <div class="relative">
-        <password-input id="new-password" placeholder=" " class="w-full peer" v-model.trim="form.password" />
-        <label class="floating-label" for="new-password">Neues Passwort (8 Zeichen oder mehr)</label>
-      </div>
-      <div class="relative">
-        <password-input id="new-password-repeat" placeholder=" " class="w-full peer" v-model.trim="form.passwordRepeat" />
-        <label class="floating-label" for="new-password-repeat">Neues Passwort wiederholen</label>
-      </div>
-      <div class="flex justify-center">
-        <submit-button @click="submit">Neues Passwort bestätigen</submit-button>
-      </div>
+      <template v-if="success">
+        <div class="text-success font-bold text-center">Das Passwort wurde erfolgreich neu gesetzt.</div>
+        <div class="text-center"><router-link :to="{ name: 'login' }" class="link">Zum Login...</router-link></div>
+      </template>
+      <template v-else>
+        <div class="relative">
+          <password-input id="new-password" placeholder=" " class="w-full peer" v-model.trim="form.password" />
+          <label class="floating-label" for="new-password">Neues Passwort (8 Zeichen oder mehr)</label>
+        </div>
+        <div class="relative">
+          <password-input id="new-password-repeat" placeholder=" " class="w-full peer" v-model.trim="form.passwordRepeat" />
+          <label class="floating-label" for="new-password-repeat">Neues Passwort wiederholen</label>
+        </div>
+        <div class="flex justify-center">
+          <submit-button @click="submit">Neues Passwort bestätigen</submit-button>
+        </div>
+      </template>
     </div>
   </div>
 </template>
