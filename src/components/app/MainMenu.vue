1f17d647eb6a7fc52fe146f98a80e6cc65bf795f
Gregor Kofler <info@gregorkofler.com>
2025-05-18 17:11:46 +0200

    Refactor: Moved active menu item styling to CSS classes

--- src/components/app/MainMenu.vue
+++ src/components/app/MainMenu.vue
@@ -17,9 +17,7 @@
             userRoles.filter(item => route.meta.roles.includes(item)).length
         "
         :to="route.name ? { name: route.name } : { path: route.path }"
-        :class="['flex items-center px-2 py-2 text-base font-medium rounded-sm items-center space-x-2 flex-nowrap overflow-hidden',
-                 $route.matched[0]?.path === route.path ? 'bg-vxvue-500 text-white' : 'text-slate-100 hover:bg-vxvue-700'
-        ]"
+        class="flex items-center px-2 py-2 text-base font-medium rounded-sm space-x-2 flex-nowrap overflow-hidden menu-item"
       >
         <component :is="route.meta.icon" v-if="route.meta.icon" class="shrink-0 size-8" />
         <span v-if="expanded">{{ route.meta.label }}</span>
